3a0e6bff86239f30efdd0e4ab7a3fcf4f21c3422
just_a_mango <horace.hoff@gmail.com>
2024-09-07 11:32:07 +0200

    /

--- convert.py
+++ convert.py
@@ -29,7 +29,7 @@ response = ollama.chat(
     messages=[
         {
             'role': 'user',
-            'content': 'Provide a detailed description for this photo without interpretations. Skip using word "image". No interpretations.',
+            'content': 'Provide a very detailed and precise description for this photo without interpretations or assumptions.',
             'images': [image_path]
         }
     ]
