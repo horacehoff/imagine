a7bcfc733ee01ef6816b4d8d8867fc76a5ada41d
Just_a_Mango <horace.hoff@gmail.com>
2024-09-14 00:40:55 +0200

    /

--- text2img.py
+++ text2img.py
@@ -7,8 +7,11 @@ Goal here is to take a .imagine file as an input and feed the whole file to an i
 import torch
 from diffusers import FluxPipeline
 
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.float8_e5m2)
 pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+pipe.vae.enable_slicing()
+pipe.vae.enable_tiling()
+pipe.to(torch.float8_e5m2)
 
 prompt = "The image presents a clear view of the moon, captured in a state of partial illumination against the backdrop of a dark sky. The moon is positioned centrally in the frame, occupying a significant portion of the visual space. Its surface is detailed, showcasing a variety of craters and maria, which are the dark, flat areas on the lunar surface. The moon's surface appears textured, with a range of shades from white to gray, indicating the presence of different materials and elevations. The surrounding sky is uniformly dark, providing a stark contrast that accentuates the moon's luminosity. There are no other celestial bodies or objects visible in the image, and no additional context or background story is provided. The image is a straightforward representation of the moon in its natural environment, captured in a moment of celestial beauty"
 image = pipe(
